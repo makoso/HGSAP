e36f9800c9d2d5355c097a92cb1ee186bb6df828
maku <kksmakoso@gmail.com>
2016-03-30 20:08:00 +0200

    Working animations on load
    Add properies:
    data-g-time
    data-g-to
    data-g-position

--- js/hgsap.js
+++ js/hgsap.js
@@ -1,9 +1,5 @@
 /**
  * Created by Krzysztof Makowski on 2016-03-25.
- * TIMELINE
- tl = new TimelineLite();
-
- TweenLite.to(photo, 2, {width:"200px", height:"150px"});
  */
 var HGSAP = function(){
     var self = this;
@@ -12,9 +8,6 @@ var HGSAP = function(){
         onScroll:[]
     };
     self.timelines = [];
-
-
-
     function makeAnimationOnLoad(){
         for( var i in self.allObiects.onLoad){
             var obj = self.allObiects.onLoad[i];
@@ -22,19 +15,21 @@ var HGSAP = function(){
             if(!index) index = 1;
             if(self.timelines[index]){
                 var objTimeline = self.timelines[index];
-                console.log('istnieje');
+                makeAnimOnTimeline(obj, objTimeline);
             } else {
-                console.log('nowa');
                 self.timelines[index] = new TimelineLite();
+                makeAnimOnTimeline(obj, self.timelines[index]);
             }
         }
     }
-
-
-
-
-
-
+    function makeAnimOnTimeline(obj, timeline){
+        var data = obj.data();
+        if(data.gFrom){
+            timeline.fromTo(obj, data.gTime, {css:data.gFrom}, {css:data.gTo}, data.gPosition);
+        } else {
+            timeline.to(obj, data.gTime, data.gTo, data.gPosition);
+        }
+    }
     this.storeObjects = function( selector ){
         self.allObiects = {
             onLoad  :[],
@@ -42,21 +37,18 @@ var HGSAP = function(){
         };
         if(!selector) selector = '[data-timeline]:not([data-finished])';
         $(selector).each(function(){
-            var obj = $(this); console.log(obj.data('on_scroll'));
+            var obj = $(this);
             if(obj.data('on_scroll')){
                 self.allObiects.onScroll.push(obj);
             } else {
                 self.allObiects.onLoad.push(obj);
             }
         });
-        console.log(self);
         makeAnimationOnLoad();
-        console.log(self);
     };
 
     $("body").bind("DOMSubtreeModified", function() {
         self.storeObjects();
-        console.log(self);
     });
 
 };
