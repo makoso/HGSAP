4481829d92e6d69963645c5d054e92e7bce74e52
maku <kksmakoso@gmail.com>
2016-03-30 19:35:52 +0200

    Split onLoad and onScroll objects
    + edit readme

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # HGSAP
 Animation written in HTML using the library GSAP ? You may soon be possible.
+# How To use
+  ## Including
+  Include GSAP
+  Download https://github.com/makoso/HGSAP/blob/master/js/hgsap.js
+  and add to your document.
+  Inside your document.ready(){} initialize hgsap with function hgsap.storeObjects();
 # About Me
 Hi, i'm a amateur programer, i like programing backend of web aplications with Symfony framework, but now i need to up my js skill, then i started this project.
--- js/hgsap.js
+++ js/hgsap.js
@@ -5,63 +5,62 @@
 
  TweenLite.to(photo, 2, {width:"200px", height:"150px"});
  */
-var XYZ = function(){
+var HGSAP = function(){
     var self = this;
-    this.allObiects = [];
-    this.tree = [];
-    this.kk = function(){
-        for(var i in this.allObiects){
-            makeTree(this.allObiects[i].obj);
-        }
-        return this;
+    self.allObiects = {
+        onLoad  :[],
+        onScroll:[]
     };
-    function makeTree( obj ){
-        if(obj.parent().attr('data-index')){
-            searchInTree(obj.parent().attr('data-index')).childrens.push({
-                index       :obj.attr('data-index'),
-                obj         :obj,
-                childrens   : []
-            });
-        } else {
-            self.tree.push({
-                index       :obj.attr('data-index'),
-                obj         :obj,
-                childrens   : []
-            });
-        }
-    }
-    function searchInTree( index, branch, abs ){
-        if(!branch) branch = self.tree;
-        var r = branch.filter(function( obj ) {
-                return obj.index == index;
-        });
-        if(r.length !== 0){
-            return r[0];
-        } else {
-            if (abs) {
-                if(abs == 2){return false;}
-                return searchInTree(index, searchInTree($('[data-index="' + index + '"]').parent().attr('data-index'),abs + 1).childrens);
+    self.timelines = [];
+
+
+
+    function makeAnimationOnLoad(){
+        for( var i in self.allObiects.onLoad){
+            var obj = self.allObiects.onLoad[i];
+            var index = obj.data('timeline');
+            if(!index) index = 1;
+            if(self.timelines[index]){
+                var objTimeline = self.timelines[index];
+                console.log('istnieje');
+            } else {
+                console.log('nowa');
+                self.timelines[index] = new TimelineLite();
             }
         }
     }
-    $('[data-timeline]').each(function(){
-        var obj = $(this);
-        var data = obj.data();
-        if(!data.index) {
-            var index = self.allObiects.length + 1;
-            obj.attr('data-index', index);
-            self.allObiects.push({index:index,obj:obj});
-        }
-    });
-    return self;
-};
-$(function() {
-    var x = new XYZ();
-    console.log(x);
+
+
+
+
+
+
+    this.storeObjects = function( selector ){
+        self.allObiects = {
+            onLoad  :[],
+            onScroll:[]
+        };
+        if(!selector) selector = '[data-timeline]:not([data-finished])';
+        $(selector).each(function(){
+            var obj = $(this); console.log(obj.data('on_scroll'));
+            if(obj.data('on_scroll')){
+                self.allObiects.onScroll.push(obj);
+            } else {
+                self.allObiects.onLoad.push(obj);
+            }
+        });
+        console.log(self);
+        makeAnimationOnLoad();
+        console.log(self);
+    };
+
     $("body").bind("DOMSubtreeModified", function() {
-        var y = x.kk();
-        console.log(y);
+        self.storeObjects();
+        console.log(self);
     });
-    //XYZ.someFn();
-    //XYZ.someProperty;
+
+};
+var hgsap = new HGSAP();
+$(function() {
+    hgsap.storeObjects();
 });
